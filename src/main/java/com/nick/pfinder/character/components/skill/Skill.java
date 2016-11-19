ce2bd15258ebd9189b8e9b7ebe9d565ce47767e7
nickyrayray <Enterprise.1701>
2016-11-19 17:13:27 -0500

    Created Skill.java, and created constants for each skill name

---
+++ src/main/java/com/nick/pfinder/character/components/skill/Skill.java
@@ -0,0 +1,44 @@
+package com.nick.pfinder.character.components.skill;
+
+/**
+ * Created by nickyrayray on 11/19/16.
+ */
+public class Skill {
+
+    public static final String ACROBATICS = "Acrobatics";
+    public static final String APPRAISE = "Appraise";
+    public static final String BLUFF = "Bluff";
+    public static final String CLIMB = "Climb";
+    public static final String CRAFT = "Craft";
+    public static final String DIPLOMACY = "Diplomacy";
+    public static final String DISABLE_DEVICE = "Disable Device";
+    public static final String DISGUISE = "Disguise";
+    public static final String ESACAPE_ARTIST = "Escape Artist";
+    public static final String FLY = "Fly";
+    public static final String HANDLE_ANIMAL = "Handle Animal";
+    public static final String HEAL = "Heal";
+    public static final String INTIMIDATE = "Intimidate";
+    public static final String KNOWLEDGE_ARCANA = "Knowledge Arcana";
+    public static final String KNOWLEDGE_DUNGEON = "Knowledge Dungeon";
+    public static final String KNOWLEDGE_ENGINEERING = "Knowledge Engineering";
+    public static final String KNOWLEDGE_GEOGRAPHY = "Knowledge Geography";
+    public static final String KNOWLEDGE_HISTORY = "Knowledge History";
+    public static final String KNOWLEDGE_LOCAL = "Knowledge Local";
+    public static final String KNOWLEDGE_NATURE = "Knowledge Nature";
+    public static final String KNOWLEDGE_NOBILITY = "Knowledge Nobility";
+    public static final String KNOWLEDGE_PLANES = "Knowledge Planes";
+    public static final String KNOWLEDGE_RELIGION = "Knowledge Religion";
+    public static final String LINGUISTICS = "Linguistics";
+    public static final String PERCEPTION = "Perception";
+    public static final String PERFORM = "Perform";
+    public static final String PROFESSION = "Profession";
+    public static final String Ride = "RIDE";
+    public static final String SENSE_MOTIVE = "Sense Motive";
+    public static final String SLEIGHT_OF_HAND = "Sleight Of Hand";
+    public static final String SPELLCRAFT = "Spellcraft";
+    public static final String STEALTH = "Stealth";
+    public static final String SURVIVAL = "Survival";
+    public static final String SWIM = "Swim";
+    public static final String USE_MAGIC_DEVICE = "Use Magic Device";
+
+}
